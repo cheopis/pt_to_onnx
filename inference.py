3b2936a68078bc5b94406b7c40aa713565b15346
cheopis <32071966+cheopis@users.noreply.github.com>
2024-11-01 18:39:57 -0300

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,9 +1,7 @@
 import os
 import cv2
 from pathlib import Path
-
-from yolov9 import YOLOv9
-
+from ultralytics import YOLO
 
 def get_detector(args):
     weights_path = args.weights
@@ -19,16 +17,28 @@ def get_detector(args):
     elif args.video:
         cap = cv2.VideoCapture(source_path)
         w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    detector = YOLOv9(model_path=weights_path,
-                      class_mapping_path=classes_path,
-                      original_size=(w, h),
-                      score_threshold=args.score_threshold,
-                      conf_thresold=args.conf_threshold,
-                      iou_threshold=args.iou_threshold,
-                      device=args.device)
+        h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))'
+        detector = YOLO(weights_path) # weights in .pt
     return detector
 
+def predict(chosen_model, img, classes=[], conf = 0.5):
+    if classes:
+        results = chosen_model.predict(img, classes=classes, conf=conf)
+    else:
+        results = chosen_model.predict(img, conf=conf)
+
+    return results
+
+def predict_and_detect(chosen_model, img, classes=[], conf=0.5, rectangle_thickness=2, text_thickness=1):
+    results = predict(chosen_model, img, classes, conf=conf)
+    for result in results:
+        for box in result.boxes:
+            cv2.rectangle(img, (int(box.xyxy[0][0]), int(box.xyxy[0][1])),
+                          (int(box.xyxy[0][2]), int(box.xyxy[0][3])), (255, 0, 0), rectangle_thickness)
+            cv2.putText(img, f"{result.names[int(box.cls[0])]}",
+                        (int(box.xyxy[0][0]), int(box.xyxy[0][1]) - 10),
+                        cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), text_thickness)
+    return img, results
 
 def inference_on_image(args):
     print("[INFO] Intialize Model")
@@ -47,24 +57,31 @@ def inference_on_image(args):
         cv2.imshow("Result", image)
         cv2.waitKey(0)
 
+def save_results(results):
+    print(results[0].boxes)
+    pass
 
 def inference_on_video(args):
     print("[INFO] Intialize Model")
-    detector = get_detector(args)
+    #detector = get_detector(args)
 
     cap = cv2.VideoCapture(args.source)
     w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     video_fps = int(cap.get(cv2.CAP_PROP_FPS))
     writer = cv2.VideoWriter('output/result.avi', cv2.VideoWriter_fourcc(*'MJPG'), video_fps, (w, h))
+    
+    model = YOLO(args.weights)
 
     print("[INFO] Inference on Video")
     while True:
         ret, frame = cap.read()
         if not ret:
             break
-        detections = detector.detect(frame)
-        detector.draw_detections(frame, detections=detections)
+        #detections = detector.detect(frame)
+        #detector.draw_detections(frame, detections=detections)
+        frame, results = predict_and_detect(model, frame, classes=None, conf=0.3, rectangle_thickness=2, text_thickness=1)
+        save_results(results)
         writer.write(frame)
         cv2.imshow("Result", frame)
         key = cv2.waitKey(1) & 0xFF
@@ -80,15 +97,13 @@ if __name__ == "__main__":
 
     parser.add_argument("--source", type=str, required=True, help="Path to image or video file")
     parser.add_argument("--weights", type=str, required=True, help="Path to yolov9 onnx file")
-    parser.add_argument("--classes", type=str, required=True, help="Path to list of class in yaml file")
+    parser.add_argument("--classes", type=str, required=False, help="Path to list of class in yaml file")
     parser.add_argument("--score-threshold", type=float, required=False, default=0.1)
     parser.add_argument("--conf-threshold", type=float, required=False, default=0.4)
     parser.add_argument("--iou-threshold", type=float, required=False, default=0.4)
     parser.add_argument("--image", action="store_true", required=False, help="Image inference mode")
     parser.add_argument("--video", action="store_true", required=False)
     parser.add_argument("--show", required=False, type=bool, default=True, help="Show result on pop-up window")
-    parser.add_argument("--device", type=str, required=False, help="Device use (cpu or cude)", choices=["cpu", "cuda"],
-                        default="cpu")
 
     args = parser.parse_args()
 
